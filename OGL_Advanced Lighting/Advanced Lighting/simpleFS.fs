ab63e3e651b42692ebdf8ae595dbeb38db958666
天线无双 <1069911023@qq.com>
2019-09-01 20:41:07 +0800

    Update simpleFS.fs

--- OGL_Advanced Lighting/Advanced Lighting/simpleFS.fs
+++ OGL_Advanced Lighting/Advanced Lighting/simpleFS.fs
@@ -15,27 +15,27 @@ uniform vec3 viewPos;
 void main()
 {
     vec3 color = texture(floorTexture, fs_in.TexCoords).rgb;
+    
+    
     // ambient
     vec3 ambient = 0.05 * color;
+    
+    
     // diffuse
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     vec3 normal = normalize(fs_in.Normal);
     float diff = max(dot(lightDir, normal), 0.0);
     vec3 diffuse = diff * color;
+    
+    
     // specular
     vec3 viewDir = normalize(viewPos - fs_in.FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);
     float spec = 0.0;
-//    if(blinn)
-//    {
-        vec3 halfwayDir = normalize(lightDir + viewDir);
-        spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
-//    }
-//    else
-//    {
-//        vec3 reflectDir = reflect(-lightDir, normal);
-//        spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
-//    }
-    vec3 specular = vec3(0.3) * spec; // assuming bright white light color
+    
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0); // 半程向量 · 法向量
+    vec3 specular = vec3(0.3) * spec; // 假设明亮的白色光
+
+    
     FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
