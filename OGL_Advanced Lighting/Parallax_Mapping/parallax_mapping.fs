60da3a2f8299011bcdc1076891942eb8d8ce1790
天线无双 <1069911023@qq.com>
2019-09-22 17:30:23 +0800

    Add files via upload

--- OGL_Advanced Lighting/Parallax_Mapping/parallax_mapping.fs
+++ OGL_Advanced Lighting/Parallax_Mapping/parallax_mapping.fs
@@ -15,39 +15,42 @@ uniform sampler2D depthMap;
 
 uniform float heightScale;
 
-vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
+//视差映射
+vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)//（输出：视差偏移后的uv坐标）（输入：透视矫正插值后的uv，观察向量）
 {
-    float height =  texture(depthMap, texCoords).r;
-    return texCoords - viewDir.xy * (height * heightScale);
+    float height =  texture(depthMap, texCoords).r; //获得此片元对应 深度图 的 深度值
+    return texCoords - viewDir.xy * (height * heightScale); //将观察向量缩放到 深度值 大小，再用此片元的 uv - 观察向量的xy（viewDir指向相机，所以 - viewDir 指向片元），得到偏移后的 uv坐标。
 }
 
 void main()
 {
-    // offset texture coordinates with Parallax Mapping
-    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos);
-    vec2 texCoords = fs_in.TexCoords;
-    
-    texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir);
+    // 使用视差贴图偏移纹理坐标 offset texture coordinates with Parallax Mapping
+    vec3 viewDir = normalize(fs_in.TangentViewPos - fs_in.TangentFragPos); //观察向量（指向相机）
+    vec2 texCoords = fs_in.TexCoords; //顶点插值后的uv坐标
+    texCoords = ParallaxMapping(fs_in.TexCoords,  viewDir); //得到 视差偏移后的uv坐标
     if(texCoords.x > 1.0 || texCoords.y > 1.0 || texCoords.x < 0.0 || texCoords.y < 0.0)
-        discard;
+        discard; //如果uv越界，则退出片段着色器
     
-    // obtain normal from normal map
-    vec3 normal = texture(normalMap, texCoords).rgb;
-    normal = normalize(normal * 2.0 - 1.0);
     
-    // get diffuse color
+    // 从法线贴图获取法线 obtain normal from normal map
+    vec3 normal = texture(normalMap, texCoords).rgb;//【0，1】
+    normal = normalize(normal * 2.0 - 1.0);//【-1，1】
+    
+    
+    // 从纹理贴图获得 漫反射颜色 get diffuse color
     vec3 color = texture(diffuseMap, texCoords).rgb;
-    // ambient
+    // 环境光 ambient
     vec3 ambient = 0.1 * color;
-    // diffuse
+    // 漫反射颜色贡献 diffuse
     vec3 lightDir = normalize(fs_in.TangentLightPos - fs_in.TangentFragPos);
-    float diff = max(dot(lightDir, normal), 0.0);
+    float diff = max(dot(lightDir, normal), 0.0); //漫反射分量（法向量 · 光方向）（大于0）
     vec3 diffuse = diff * color;
-    // specular
+    // 镜面反射颜色贡献 specular
     vec3 reflectDir = reflect(-lightDir, normal);
     vec3 halfwayDir = normalize(lightDir + viewDir);
-    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
-    
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0); //镜面反射分量：【 (相机方向 · 反射方向)^32 】
     vec3 specular = vec3(0.2) * spec;
+    
+    
     FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
--- OGL_Advanced Lighting/Parallax_Mapping/parallax_mapping.vs
+++ OGL_Advanced Lighting/Parallax_Mapping/parallax_mapping.vs
@@ -6,11 +6,11 @@ layout (location = 3) in vec3 aTangent;
 layout (location = 4) in vec3 aBitangent;
 
 out VS_OUT {
-    vec3 FragPos;
-    vec2 TexCoords;
-    vec3 TangentLightPos;
-    vec3 TangentViewPos;
-    vec3 TangentFragPos;
+    vec3 FragPos; //片元坐标（世界空间）
+    vec2 TexCoords; //uv坐标
+    vec3 TangentLightPos; //光源坐标（切线空间）
+    vec3 TangentViewPos; //观察坐标（切线空间）
+    vec3 TangentFragPos; //片元坐标（切线空间）
 } vs_out;
 
 uniform mat4 projection;
@@ -25,14 +25,15 @@ void main()
     vs_out.FragPos = vec3(model * vec4(aPos, 1.0));
     vs_out.TexCoords = aTexCoords;
     
+    //世界空间 切线向量，副切线向量，法线向量
     vec3 T = normalize(mat3(model) * aTangent);
     vec3 B = normalize(mat3(model) * aBitangent);
     vec3 N = normalize(mat3(model) * aNormal);
-    mat3 TBN = transpose(mat3(T, B, N));
+    mat3 TBN = transpose(mat3(T, B, N));//TBN矩阵的逆矩阵（正交矩阵的置换矩阵=逆矩阵）用来把其他相关向量转换到切线空间
     
-    vs_out.TangentLightPos = TBN * lightPos;
-    vs_out.TangentViewPos  = TBN * viewPos;
-    vs_out.TangentFragPos  = TBN * vs_out.FragPos;
+    vs_out.TangentLightPos = TBN * lightPos; //光源坐标（世界空间->切线空间）
+    vs_out.TangentViewPos  = TBN * viewPos; //观察坐标（世界空间->切线空间）
+    vs_out.TangentFragPos  = TBN * vs_out.FragPos; //片元坐标（世界空间->切线空间）
     
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0); //片元坐标（裁剪空间）
 }
