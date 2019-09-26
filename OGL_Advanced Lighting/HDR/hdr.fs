fe8a8a3aba2020ef266f9750dd64831c36f90c0e
天线无双 <1069911023@qq.com>
2019-09-27 01:14:48 +0800

    Add files via upload

--- OGL_Advanced Lighting/HDR/hdr.fs
+++ OGL_Advanced Lighting/HDR/hdr.fs
@@ -13,11 +13,14 @@ void main()
     vec3 hdrColor = texture(hdrBuffer, TexCoords).rgb;
     if(hdr)
     {
-        // reinhard
+        // Reinhard色调映射
         // vec3 result = hdrColor / (hdrColor + vec3(1.0));
-        // exposure
+        
+        // exposure 曝光色调映射。exposure越小，明亮区域细节越丰富；exposure越大，黑暗部分细节越丰富
         vec3 result = vec3(1.0) - exp(-hdrColor * exposure);// e^(-hdrColor * exposure)
-        // also gamma correct while we're at it
+        
+        
+        // gamma矫正 also gamma correct while we're at it
         result = pow(result, vec3(1.0 / gamma));
         FragColor = vec4(result, 1.0);
     }
